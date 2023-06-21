bbfe621b4967c0d371ddea9319d09368f73b464f
LucasGuedes-s <lucasguedes2908@gmail.com>
2023-06-21 10:47:32 -0300

    first commit

---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+      <div class="nav">
+        <img src="@/assets/logo.png" alt="Nav Image">
+        <div class="text-nav">
+          <router-link to="/pedido"><a>Realizar Pedido</a></router-link>
+          <router-link to="/"><a>Contatos</a></router-link>
+          <router-link to="/"><a>Sobre nós</a></router-link>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+<style>
+  .nav {
+    position: absolute;
+    width: 100%;
+    margin: 0;
+    margin-bottom: 200px; /* ajuste o valor conforme necessário */
+    display: flex;
+    align-items: center;
+  }
+  .nav img {
+    float: left;
+    height: 150px;
+    margin-left: 50px;
+  }
+  .nav a {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 19px;
+    text-decoration: none;
+    color: #9666ca;
+    margin-right: 20px;
+  }
+  .text-nav{
+    margin-left: 50px;
+  }
+</style>
+  
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <NavBar />
+
+    <div class="home-inicio">
+    </div>
+  </div>
+
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar
+  }
+}
+</script>
+<style>
+ .home-inicio {
+  position: absolute;
+  top: 100px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.home-inicio img {
+  width: 70%;
+}
+</style>
